18708d1c8f27d13e6058b01b658570e7a45dab86
David Rawalia <davidrawalia@gmail.com>
2019-09-08 18:05:43 -0400

    Router: implement M4L to C++ UDP communication

---
+++ Router/c++/Aesthesia-router/Aesthesia-router/pch.h
@@ -0,0 +1,12 @@
+#ifndef PCH_H
+#define PCH_H
+
+#include <winsock2.h>
+#include <windows.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <string>
+
+#endif
