2c508601e99426b8af3ec9812e49db9a8f729117
David Rawalia <davidrawlia@gmail.com>
2020-07-12 20:33:26 -0400

    Visualizer: Integrate assimp libraries

--- Visualizer/Aesthesia-visualizer/Aesthesia-visualizer/Aesthesia-visualizer.h
+++ Visualizer/Aesthesia-visualizer/Aesthesia-visualizer/Aesthesia-visualizer.h
@@ -5,17 +5,14 @@
 #include "Object.h"
 #include "Cube.h"
 
-bool debug = false;
-const GLuint WINDOW_WIDTH = 1000, WINDOW_HEIGHT = 1000;
+const bool debug = false;
+const bool fps_count = false;
+
+const GLuint WIN_WIDTH = 1000, WIN_HEIGHT = 1000;
 GLFWwindow* window;
-GLclampf* redbk = new GLclampf;
-GLclampf* greenbk = new GLclampf;
-GLclampf* bluebk = new GLclampf;
-glm::vec3 horsePosition = glm::vec3(0.0f, 0.0f, 0.0f);
-glm::vec3 horseScale = glm::vec3(1.0f, 1.0f, 1.0f);
-GLfloat horseYRotation = 0.0f;
-GLfloat horseZRotation = 0.0f;
-std::vector <float> horseJointRotation;
+GLclampf* redbk = new GLclampf(0.5f);
+GLclampf* greenbk = new GLclampf(0.5f);
+GLclampf* bluebk = new GLclampf(0.5f);
 GLfloat worldYRotation = 0.0f;
 GLfloat worldXRotation = 0.0f;
 glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
@@ -23,17 +20,17 @@ glm::vec3 right = glm::vec3(-1.0f, 0.0f, 0.0f);
 glm::vec3 front = glm::vec3(0.0f, 0.0f, -1.0f);
 GLdouble lastTime = glfwGetTime();
 GLuint nbFrames = 0;
-Camera camera = Camera::Camera(0.0f, 30.0f, 0.0f, 0.0f, 1.0f, 0.0f, -90.0f, -89.0f);
-Camera lightCamera = Camera::Camera(0.0f, 20.0f, 0.0f, 0.0f, 1.0f, 0.0f, -90.0f, -89.0f);
+Camera camera = Camera(0.0f, 30.0f, 0.0f, 0.0f, 1.0f, 0.0f, -90.0f, -89.0f);
+GLfloat ambLight;
 
-GLfloat lastX = WINDOW_WIDTH / 2.0f;
-GLfloat lastY = WINDOW_WIDTH / 2.0f;
+GLfloat lastX = WIN_WIDTH / 2.0f;
+GLfloat lastY = WIN_WIDTH / 2.0f;
 GLfloat deltaTime = 0.0f;
 GLfloat lastFrame = 0.0f;
 bool keys[1024];
 bool mousePressed[3];
-int width = WINDOW_WIDTH;
-int height = WINDOW_HEIGHT;
+int width = WIN_HEIGHT;
+int height = WIN_HEIGHT;
 GLenum renderMode = GL_FILL;
 GLfloat textureBool = 1.0;
 
