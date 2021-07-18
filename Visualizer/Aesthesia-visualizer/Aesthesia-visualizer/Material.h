cf643bf6f73838520f9f368c2a45d031ce220d0d
David Rawalia <davidrawalia@gmail.com>
2021-07-18 18:26:17 -0400

    Visualizer: add support for texture-less materials

--- Visualizer/Aesthesia-visualizer/Aesthesia-visualizer/Material.h
+++ Visualizer/Aesthesia-visualizer/Aesthesia-visualizer/Material.h
@@ -6,9 +6,12 @@ class Material {
 public:
 	Material(Mesh* mesh);
 	GLuint getTexture(int i);
+	bool hasTexture(int i);
+
 private:
 	aiString* textureFilePath;
 	std::vector<GLuint> textures;
+	std::vector<bool> haveTextures;
 	std::vector<int> texWidths;
 	std::vector<int> texHeights;
 };
